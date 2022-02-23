a6e050d27cb4834dc1b95e163743c8fa4a05fbcb
MINGYONG YAO <yaomingyong@fullerstock.com>
2022-02-23 18:25:44 +0800

    更新订单执行器

--- order/executor.py
+++ order/executor.py
@@ -5,6 +5,7 @@ from credential import Credential
 from events import default_event_source_conf
 from events.event import OrderEvent
 from events.event_source import EventSourceConfig
+from order.utils import OkexOrderIDGenerator
 from types import OrderType
 from third_party.okex.client import Client as OkexClient
 
@@ -18,9 +19,11 @@ class OkexOrderExecutor:
     def __init__(self, credential: Credential):
         super().__init__(credential)
         self.client = OkexClient(credential.api_key, credential.secret_key, credential.phrase)
+        self.order_id_generator = OkexOrderIDGenerator()
 
     def create_order(self, *args, **kwargs):
-        pass
+        client_order_id = self.order_id_generator.gen()
+        # todo
 
 
 class BinanceOrderExecutor:
--- order/utils.py
+++ order/utils.py
@@ -1,3 +1,4 @@
+import time
 from abc import ABC, abstractmethod
 
 
@@ -7,8 +8,10 @@ class OrderIDGenerator(ABC):
 
 
 class OkexOrderIDGenerator(OrderIDGenerator):
+    """字母（区分大小写）与数字的组合，可以是纯字母、纯数字且长度要在1-32位之间。"""
+
     def gen(self):
-        pass
+        return int(time.time()*10**6)
 
 
 class BinanceOrderIDGenerator(OrderIDGenerator):
